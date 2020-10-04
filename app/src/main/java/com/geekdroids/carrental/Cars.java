5661f711c089fc9cc28ee92b4206556b29a4c9ad
thinu <thinuramf@gmail.com>
2020-10-04 21:05:34 +0530

    add cars

---
+++ app/src/main/java/com/geekdroids/carrental/Cars.java
@@ -0,0 +1,41 @@
+package com.geekdroids.carrental;
+
+public class Cars {
+    private String Model;
+    private String Owner;
+    private String ID;
+
+    public Cars(){
+
+    }
+
+    public Cars(String model, String owner, String ID) {
+        Model = model;
+        Owner = owner;
+        this.ID = ID;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+}
